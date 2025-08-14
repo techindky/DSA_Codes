00b04021429622206c3d6feec018448be4c8cf19
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-14 17:58:43 +0530

    Create FindPivotIndex.py

---
+++ LeetCode/FindPivotIndex.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        leftsum, rightsum = 0, sum(nums)
+        for i in range(len(nums)):
+          rightsum-=nums[i]
+          if leftsum==rightsum:
+            return i
+          leftsum+=nums[i]
+        return -1
