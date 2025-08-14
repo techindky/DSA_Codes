e177b87c0c462653bb98dfa14aa6646c04a39562
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-14 17:14:53 +0530

    Create MoveZeroes.py

---
+++ LeetCode/MoveZeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j]!=0:
+                nums[j], nums[i] = nums[i], nums[j]
+                i+=1
+            
+        
