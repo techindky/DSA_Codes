f0ce3d7143bb860ba77670768bacada7568a5850
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-14 23:48:51 +0530

    Create SortAnArray.java

---
+++ LeetCode/SortAnArray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums);
+        return nums;
+    }
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) return;
+        int mid = arr.length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            boolean condition = left[i] <= right[j];
+            arr[k++] = condition ? left[i++] : right[j++];
+        }
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+    }
+}
