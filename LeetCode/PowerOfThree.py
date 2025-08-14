028aa37221255bc5306a91161cc413093c2f9790
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-14 15:29:40 +0530

    Update PowerOfThree.py

--- LeetCode/PowerOfThree.py
+++ LeetCode/PowerOfThree.py
@@ -1,9 +1,3 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if (n < 1):
-            return False
-        while (n > 1):
-            if n % 3 != 0:
-                return False
-            n //= 3
-        return True
+        return n > 0 and 1162261467 % n == 0
