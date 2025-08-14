7e0932017f03001bbce517001d13f9e0d5a8b61a
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-15 01:01:24 +0530

    Create MaximumHeightOfATriangle.py

---
+++ LeetCode/MaximumHeightOfATriangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxHeightOfTriangle(self, red: int, blue: int) -> int:
+        if red < blue:
+            return self.maxHeightOfTriangle(blue, red)
+        h1, h2 = isqrt(blue*4+1), isqrt(blue)*2
+        if (h1+1)**2//4 <= red: return h1
+        if (h1+1)**2//4 <= red:     return h1
+        if ((h2+1)**2-1)//4 <= red: return h2
+        if (h1-1)**2//4 <= red:     return h1-1
+        return h2-1
