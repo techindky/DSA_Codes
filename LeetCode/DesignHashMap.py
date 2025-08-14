fc6599e14c90286eac18f3597cb248d37ad5a184
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-14 17:55:49 +0530

    Create DesignHashMap.py

---
+++ LeetCode/DesignHashMap.py
@@ -0,0 +1,40 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 1000
+        self.table = [[] for _ in range(self.size)]
+
+    def _hash(self, key):
+        return hash(key) % self.size
+
+    def put(self, key: int, value: int) -> None:
+        index = self._hash(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+        self.table[index].append([key,value])
+
+    def get(self, key: int) -> int:
+        index = self._hash(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                return pair[1]
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        for i, pair in enumerate(self.table[index]):
+            if pair[0] == key:
+                del self.table[index][i]
+                return
+        return None
+
+    def __repr__(self):
+        return str(self.table)
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
