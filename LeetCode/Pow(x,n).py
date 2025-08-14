4053d347a5691e32550dc7eae4cd288f7082b537
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-14 13:10:59 +0530

    Create Pow(x,n).py

---
+++ LeetCode/Pow(x,n).py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        temp = x
+        if n == 0:
+            return 1
+        elif n < 0:
+            x = 1 / x
+            n = -n
+        half = self.myPow(x, n // 2)
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
