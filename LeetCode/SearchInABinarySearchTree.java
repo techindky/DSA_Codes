3196e311c3a882542536beeb9823f5a50036d40a
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-21 18:24:02 +0530

    Create SearchInABinarySearchTree.java

---
+++ LeetCode/SearchInABinarySearchTree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.val == val) return cur;
+            cur = val < cur.val ? cur.left : cur.right;
+        }
+        return null;
+    }
+}
