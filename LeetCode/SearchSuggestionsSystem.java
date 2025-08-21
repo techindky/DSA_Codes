c5b0332bbc896682392f3e6b5e16f7a0011dde7a
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-21 19:00:50 +0530

    Update SearchSuggestionsSystem.java

--- LeetCode/SearchSuggestionsSystem.java
+++ LeetCode/SearchSuggestionsSystem.java
@@ -1,3 +1,34 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Arrays.sort(products); 
+
+        List<List<String>> result = new ArrayList<>();
+        int left = 0, right = products.length - 1;
+        int len = searchWord.length();
+
+        for (int i = 0; i < len; i++) {
+            char c = searchWord.charAt(i);
+            while (left <= right && (products[left].length() <= i || products[left].charAt(i) != c)) {
+                left++;
+            }
+            while (left <= right && (products[right].length() <= i || products[right].charAt(i) != c)) {
+                right--;
+            }
+            List<String> suggestions = new ArrayList<>();
+            for (int j = left; j <= right && j < left + 3; j++) {
+                suggestions.add(products[j]);
+            }
+
+            result.add(suggestions);
+        }
+
+        return result;
+    }
+}
+
+
 // Custom class Trie with function to get 3 words starting with given prefix
 class Trie {
 
