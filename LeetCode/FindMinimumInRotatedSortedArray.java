03b28dbeead8b6f9415cee47831c7f1e151a37f1
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-21 17:10:20 +0530

    Create FindMinimumInRotatedSortedArray.java

---
+++ LeetCode/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int s = 0, e = nums.length - 1;
+        while (s < e) {
+            int mid = (s + e) / 2;
+            if (nums[mid] <= nums[e]) {
+                e = mid;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return nums[s];
+    }
+}
