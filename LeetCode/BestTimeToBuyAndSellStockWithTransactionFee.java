3a5386dca47cc28ccafea05911a56bed0574e4d2
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-21 18:51:17 +0530

    Create BestTimeToBuyAndSellStockWithTransactionFee.java

---
+++ LeetCode/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int hold = -prices;
+        int cash = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            // Calculate next states based on previous ones
+            int newHold = Math.max(hold, cash - prices[i]);
+            int newCash = Math.max(cash, hold + prices[i] - fee);
+            hold = newHold;
+            cash = newCash;
+        }
+        return cash;
+    }
+}
