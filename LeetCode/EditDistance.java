034b1ea5392ae0cbcc305dad4e66b9ce68a7dc73
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-21 18:57:09 +0530

    Update EditDistance.java

--- LeetCode/EditDistance.java
+++ LeetCode/EditDistance.java
@@ -1,27 +1,23 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length(), n = word2.length();
-        // dp[j] will represent dp[i-1][j] for previous row; curr[j] is dp[i][j]
-        int[] dp = new int[n + 1];
-        for (int j = 0; j <= n; j++) dp[j] = j; // dp[0][j] = j
+        final int m = word1.length();//first word length
+        final int n = word2.length();///second word length
+        // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
+        int[][] dp = new int[m + 1][n + 1];
 
-        for (int i = 1; i <= m; i++) {
-            int[] curr = new int[n + 1];
-            curr = i; // dp[i] = i
-            char a = word1.charAt(i - 1);
-            for (int j = 1; j <= n; j++) {
-                char b = word2.charAt(j - 1);
-                if (a == b) {
-                    curr[j] = dp[j - 1]; // no operation
-                } else {
-                    int insert = curr[j - 1] + 1;   // insert b
-                    int delete = dp[j] + 1;         // delete a
-                    int replace = dp[j - 1] + 1;    // replace a->b
-                    curr[j] = Math.min(insert, Math.min(delete, replace));
-                }
-            }
-            dp = curr;
-        }
-        return dp[n];
+        for (int i = 1; i <= m; ++i)
+            dp[i][0] = i;
+
+        for (int j = 1; j <= n; ++j)
+            dp[0][j] = j;
+
+        for (int i = 1; i <= m; ++i)
+            for (int j = 1; j <= n; ++j)
+                if (word1.charAt(i - 1) == word2.charAt(j - 1))//same characters
+                    dp[i][j] = dp[i - 1][j - 1];//no operation
+                else
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1; //replace               //delete        //insert
+
+        return dp[m][n];
     }
 }
