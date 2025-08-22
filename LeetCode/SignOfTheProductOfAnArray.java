460a97463985fe39fbffc10f673b36cd5fed498a
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-22 12:33:06 +0530

    Create SignOfTheProductOfAnArray.java

---
+++ LeetCode/SignOfTheProductOfAnArray.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int negCount = 0;
+        for (int i : nums) {
+            if (i < 0) negCount++;
+            if (i == 0) return 0;
+        }
+        return negCount % 2 == 0 ? 1 : -1;
+    }
+}
