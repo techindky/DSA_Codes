f1cb3889da8ba338a972616584bc89f4e25e0c85
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-22 21:12:37 +0530

    Create JumpGame.java

---
+++ LeetCode/JumpGame.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) return false; // cannot even reach i
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) return true; //
+        }
+        return false;
+    }
+}
