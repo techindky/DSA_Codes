9932f288128fa4e76beba4af3c38b8820107a059
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-25 22:08:49 +0530

    Create BestTimeToBuyAndSellStockIII.java

---
+++ LeetCode/BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
+        int sell1 = 0, sell2 = 0;
+
+        for (int p : prices) {
+            buy1 = Math.max(buy1, -p);
+            sell1 = Math.max(sell1, buy1 + p);
+            buy2 = Math.max(buy2, sell1 - p);
+            sell2 = Math.max(sell2, buy2 + p);
+        }
+
+        return sell2;
+    }
+}
