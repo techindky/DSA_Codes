17e6b76aacf288124d8fffb0b7daf78b25721cdd
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-25 22:19:48 +0530

    Create LongestArithmeticSubsequence.java

---
+++ LeetCode/LongestArithmeticSubsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestArithSeqLength(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer>[] dp = new HashMap[n];
+        for (int i = 0; i < n; i++) dp[i] = new HashMap<>();
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int diff = nums[i] - nums[j];
+                int len = dp[j].getOrDefault(diff, 1) + 1;
+                dp[i].put(diff, len);
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+        return maxLen;
+    }
+}
