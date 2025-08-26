39d850514ec9332fa7020c56cb4c42a5194aeafd
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-26 17:36:52 +0530

    Create FirstUniqueCharacterInAString.java

---
+++ LeetCode/FirstUniqueCharacterInAString.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> chars = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            chars.put(s.charAt(i), chars.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (chars.get(s.charAt(i)) == 1) return i;
+        }
+        return -1;
+    }
+}
