eb609906a26f3a44d7c40f581bf24e28ddc33068
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-27 18:11:23 +0530

    Create SquaresOfASortedArray.java

---
+++ LeetCode/SquaresOfASortedArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = 0, right = n - 1, idx = n - 1;
+        while (left<=right) {
+            if (Math.abs(nums[left]) > nums[right]) {
+                res[idx--] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[idx--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
