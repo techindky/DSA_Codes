b9cffe7248de3631ee467497c137511e415f9c6a
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-29 13:05:30 +0530

    Create SpiralMatrixII.java

---
+++ LeetCode/SpiralMatrixII.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int val = 1, top = 0, bottom = n - 1, left = 0, right = n - 1;
+        while(val <= n * n) {
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = val++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = val++;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                matrix[bottom][i] = val++;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                matrix[i][left] = val++;
+            }
+            left++;
+        }
+        return matrix;
+    }
+}
