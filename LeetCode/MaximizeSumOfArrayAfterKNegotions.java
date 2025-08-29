e1ad2981a5b93b3e33ac7ae4f3b1d5907de504ec
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-29 14:20:52 +0530

    Create MaximizeSumOfArrayAfterKNegotions.java

---
+++ LeetCode/MaximizeSumOfArrayAfterKNegotions.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int min = 101, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0 && k > 0) {
+                nums[i] = -nums[i];
+                k--;
+            }
+            min = Math.min(nums[i], min);
+            sum += nums[i];
+        }
+        if (min == 0) return sum;
+        if (k % 2 == 1 && min < 101) {
+            sum -= 2 * min;
+        }
+        return sum;
+    }
+}
