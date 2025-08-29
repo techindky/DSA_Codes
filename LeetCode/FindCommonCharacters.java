79f56532a0b6914b08f55bb9ca13b14aca20c052
Dinesh Yadav <53136851+techindky@users.noreply.github.com>
2025-08-29 14:00:04 +0530

    Update FindCommonCharacters.java

--- LeetCode/FindCommonCharacters.java
+++ LeetCode/FindCommonCharacters.java
@@ -1,31 +1,22 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        int n = words.length;
-        List<Map<Character, Integer>> freqList = new ArrayList<>();
+        int[] minFreq = new int[26];
+        Arrays.fill(minFreq, 101);
         for (String word : words) {
-            Map<Character, Integer> freq = new HashMap<>();
+            int[] freq = new int[26];
             for (int i = 0; i < word.length(); i++) {
                 char c = word.charAt(i);
-                freq.put(c, freq.getOrDefault(c, 0) + 1);
+                freq[c - 'a']++;
             }
-            freqList.add(freq);
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], freq[i]);
+            }
         }
         List<String> res = new ArrayList<>();
-        for (int i = 0; i < words[0].length(); i++) {
-            char key = words[0].charAt(i);
-            int value = freqList.get(0).get(key);
-            boolean found = true;
-            for (int j = 1; j < n; j++) {
-                int v = freqList.get(j).getOrDefault(key, 0);
-                if (v == 0) {
-                    found = false;
-                    break;
-                }
-                freqList.get(j).put(key, v - 1);
-            }
-            if (value > 0 && found) {
-                freqList.get(0).put(key, value - 1);
-                res.add(String.valueOf(key));
+        for (int i = 0; i < 26; i++) {
+            String val = String.valueOf((char)(i+'a'));
+            for (int j = 0; j < minFreq[i]; j++) {
+                res.add(val);
             }
         }
         return res;
